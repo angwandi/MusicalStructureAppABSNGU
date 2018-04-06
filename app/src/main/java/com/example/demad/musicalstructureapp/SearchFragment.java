253935984f72ce24ace4d144672831ce70c8024c
Innocent <31923567+angwandi@users.noreply.github.com>
2018-04-06 15:30:47 +0100

    Add search function

--- app/src/main/java/com/example/demad/musicalstructureapp/SearchFragment.java
+++ app/src/main/java/com/example/demad/musicalstructureapp/SearchFragment.java
@@ -5,6 +5,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class SearchFragment extends Fragment {
 
@@ -14,6 +19,8 @@ public class SearchFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        View rootView = inflater.inflate(R.layout.search_view, container, false);
+        // Create a list of songs
+        return rootView;
     }
 }
